8e94666b3f383c8b62949a432ff59d7c0ad34174
Arturo HURTADO MÉNDEZ <arturohurtadomendez90@gmail.com>
2026-04-09 18:02:23 -0500

    Creacion de método de sobrecarga - caso 3

--- Caso3/src/Operacion.java
+++ Caso3/src/Operacion.java
@@ -1,9 +1,32 @@
 
 public class Operacion {
+    // Primer método de sobrecarga 1: suma de 2 enteros
+	
+    int sumar(int a, int b) {
+        return a + b;
+    }
+
+    // Segundo método de sobrecarga 2: suma de 2 decimales
+    
+    double sumar(double a, double b) {
+        return a + b;
+    }
+
+    // Tercer método de sobrecarga: suma de 3 enteros
+    
+    int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Operacion sobrecarga = new Operacion();
+		
+		   System.out.println("Suma de 2 enteros: " + sobrecarga.sumar(5, 3));
+		   System.out.println("Suma de 2 decimales: " + sobrecarga.sumar(5.5, 3.2));
+		   System.out.println("Suma de 3 enteros: " + sobrecarga.sumar(1, 2, 3));
 	}
+	
 
 }
+
