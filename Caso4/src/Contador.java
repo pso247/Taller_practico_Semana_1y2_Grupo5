b8922d216f7ad98241274a75732652b16b17abd3
RonyTC <roni2829@hotmail.com>
2026-04-09 22:06:20 -0500

    agregué la resolución del caso 4

---
+++ Caso4/src/Contador.java
@@ -0,0 +1,20 @@
+
+public class Contador {
+	private static int contador=0;
+	public Contador(){
+		contador++;
+	}
+	public static int getContador() {
+		return contador;
+	}
+	
+
+	public static void main(String[] args) {
+		new Contador ();
+		new Contador ();
+		new Contador ();
+		System.out.println("Objetos Creados"+ Contador.getContador());
+
+	}
+
+}
